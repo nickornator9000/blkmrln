17119980221b35edcc3e8fb76b5fad6d0372b3e5
nickornator9000 <nicholasfulton14@gmail.com>
2024-08-10 00:30:47 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,4 +17,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    entry_points={
+        'console_scripts': [
+            'blkmrln=blkmrln.cli:main',
+        ],
+    },
 )
